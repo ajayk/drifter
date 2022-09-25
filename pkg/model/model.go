a7228751a3a376d2250a70c4002ba14b12d06af8
ajayk <ajaykemparaj@gmail.com>
2022-09-24 20:45:14 -0700

    initial structure for pkg and cmd

---
+++ pkg/model/model.go
@@ -0,0 +1,42 @@
+// Copyright © 2022 Ajay K <ajaykemparaj@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package model
+
+type KubernetesDoc struct {
+	Helm       K8sHelm    `yaml:"helm"`
+	Storage    K8sStorage `yaml:"storage"`
+	Ingress    K8sIngress `yaml:"ingress"`
+	Kubernetes Kubernetes `yaml:"kubernetes"`
+}
+
+type Kubernetes struct {
+	Namespaces []string `yaml:"namespaces"`
+}
+
+type K8sIngress struct {
+	IngressClasses []string `yaml:"classes"`
+}
+
+type K8sHelm struct {
+	Components []HelmComponent `yaml:"components"`
+}
+type HelmComponent struct {
+	Name       string `yaml:"name"`
+	Version    string `yaml:"version"`
+	AppVersion string `yaml:"appVersion"`
+}
+
+type K8sStorage struct {
+	StorageClasses []string `yaml:"classes"`
+}
