0fcddd2b226006ba49a7093406618b8e53398bb9
ajayk <ajaykemparaj@gmail.com>
2022-09-26 17:05:00 -0700

    initial e2e test

---
+++ .ci/e2e.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#####
+# This script is created to use drifter with a few basic checks in a kind cluster.
+# In the long run I hope that we can use it to run test cases.
+#####
+
+# Set NS
+NS=namespace-test
+name=namespace-test
+
+# Create namespace
+kubectl create namespace $NS
+
+sleep 2
+
+./drifter version
