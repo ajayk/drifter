83f397379ec1ccfb303fef5ae97172215a8c8c93
ajayk <ajaykemparaj@gmail.com>
2022-11-11 16:19:17 -0800

    docs: add usage video

--- .ci/e2e.sh
+++ .ci/e2e.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 #####
 # This script is created to use drifter with a few basic checks in a kind cluster.
 # In the long run I hope that we can use it to run test cases.
@@ -8,52 +10,50 @@
 # Set NS
 NS=namespace-test
 name=namespace-test
-
+kubeconfigpath=/home/runner/.kube/config
 # Create namespace
 kubectl create namespace $NS
 sleep 2
 echo ${PWD}
 go mod download
 go build
-kubectl get pods --v=6
-kubectl get ds -n kube-system
-kubectl get deployments -A
-kubectl get sc -A
+
 kubectl apply -f ${PWD}/.ci/nginx.yaml
 
+set  xtrace
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --version 5.11.0
 
+sleep 2
 ./drifter version
-./drifter check  -k  /home/runner/.kube/config -c  ${PWD}/.ci/check.yaml
 
+./drifter check  -k  ${kubeconfigpath} -c  ${PWD}/.ci/check.yaml
 if [ $? != 0 ]
 then
     echo "E2E Tests Failed ... Should have returned exit code 0"
     exit 1
 fi
 
-./drifter check  -k  /home/runner/.kube/config -c  ${PWD}/.ci/check-fail.yaml
+./drifter check  -k  ${kubeconfigpath} -c  ${PWD}/.ci/check-fail.yaml
 if [ $? != 2 ]
 then
     echo "E2E Tests Failed ... Should have returned exit code 2"
     exit 1
 fi
 
-./drifter check  -k  /home/runner/.kube/config -c  ${PWD}/.ci/check-helm-pass.yaml
+./drifter check  -k  ${kubeconfigpath} -c  ${PWD}/.ci/check-helm-pass.yaml
 if [ $? != 0 ]
 then
     echo "E2E Tests Failed ... Should have returned exit code 0"
     exit 1
 fi
 
-./drifter check  -k  /home/runner/.kube/config -c  ${PWD}/.ci/check-helm-fail.yaml
+./drifter check  -k  ${kubeconfigpath} -c  ${PWD}/.ci/check-helm-fail.yaml
 if [ $? != 2 ]
 then
     echo "E2E Tests Failed ... Should have returned exit code 0"
     exit 1
 fi
-
 
 helm delete kubernetes-dashboard
 
